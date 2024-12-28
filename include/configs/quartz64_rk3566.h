1fb04132e7c9706ad9ba7aaab9a0ccdc90ac6cc8
Dang Huynh <danct12@riseup.net>
2024-12-28 21:00:53 +0700

    add vidconsole to stdout

--- include/configs/quartz64_rk3566.h
+++ include/configs/quartz64_rk3566.h
@@ -3,7 +3,10 @@
 #ifndef __QUARTZ64_RK3566_H
 #define __QUARTZ64_RK3566_H
 
-#define ROCKCHIP_DEVICE_SETTINGS
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdin=usbkbd,serial\0" \
+		"stdout=vidconsole,serial\0" \
+		"stderr=vidconsole,serial\0"
 
 #include <configs/rk3568_common.h>
 
